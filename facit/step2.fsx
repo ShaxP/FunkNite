f28ac49ec6190363a7d5bf67bd40e16c0a9b0621
Pourmakhdomi <shahram.pourmakhdomi@cgi.com>
2018-06-08 06:47:17 +0200

    minor updates

--- facit/step2.fsx
+++ facit/step2.fsx
@@ -1,19 +1,3 @@
-let withdraw  amount transactions =
-    transactions @ [-amount]
-
-let deposit amount transcations = 
-    transcations @ [amount]
-
-let balance (transactions:int list) =
-    List.sum transactions
-
-
-// Examples
-let transactions = [30000]
-let newTransactions = transactions |> withdraw 200 |> deposit 250 |> withdraw 300 |> deposit 500 
-
-transactions |> withdraw 200 |> deposit 250 |> withdraw 300 |> deposit 500 |> balance
-
 // New concepts:
 // - Lists
 // - List append
@@ -23,8 +7,18 @@ transactions |> withdraw 200 |> deposit 250 |> withdraw 300 |> deposit 500 |> ba
 
 // Övningar
 // 1. Skriv om withdraw och deposit så att de tar en lista av transaktioner
+let withdraw  amount transactions = (-amount) :: transactions
+let deposit amount transcations = amount :: transcations
 // 2. Skriv funktionen Balance som räknar ut saldot
-// 3. Skriv en funktion som returnerar den första transaktionen
+let balance (transactions:int list) = List.sum transactions
+// 3. Skriv en funktion som returnerar den senaste transaktionen
+let latestTransaction transactions = 
+    match transactions with
+    | (head :: _) -> head
 // 4. Skriv en funktion som returnerar den största deponeringen respektive den sörsta uttaget
+let largestWithdrawal transactions = List.min transactions
+
+let largestDisposition transactions = List.max transactions
 // 5. Skriv en kodrad som utgår från balance på 3000, tar ut 200, deponerar 250, tar ut 300, deponerar 500 och returnerar saldot
+[3000] |> withdraw 200 |> deposit 250 |> withdraw 300 |> deposit 500 |> balance
 
--- facit/step4.fsx
+++ facit/step4.fsx
@@ -7,22 +7,27 @@ type Timestamp = DateTime
 type Transaction = 
     | Deposition of Amount * Timestamp
     | Withdrawal of Amount * Timestamp
+// Övningar
+// Givet typerna Amount, Timestamp och Transaction enligt ovan
 
+
+// 1. Skriv om funktionerna withdraw och deposit
 let withdraw  amount transactions =
    transactions @ [Withdrawal(amount, DateTime.Now)]
 
 let deposit amount transcations:Transaction list = 
     transcations @ [Deposition(amount, DateTime.Now)]
 
-let transactionAmount transaction =
+// 2. Skriv om funktionen balance. Tips: använd List.sumBy med en funktion som tar en Transaction och returnerar dess belopp
+let valueOf transaction =
     match transaction with
     | Deposition (amount, _) -> amount
     | Withdrawal (amount, _) -> (-amount)
 
 let balance transactions = 
-    List.sumBy (transactionAmount) transactions
+    List.sumBy (valueOf) transactions
 
-// Examples
+// 3. Testa koden
 let transactions = []
 
 transactions |> deposit 30000.0m |> withdraw 200.0m |> deposit 250.0m |> withdraw 300.0m |> deposit 500.0m |> balance
@@ -33,8 +38,3 @@ transactions |> deposit 30000.0m |> withdraw 200.0m |> deposit 250.0m |> withdra
 // - Pattern matching
 // - Discriminated unions
 
-// Övningar
-// Givet typerna Amount, Timestamp och Transaction enligt ovan
-// 1. Skriv om funktionerna withdraw och deposit
-// 2. Skriv om funktionen balance. Tips: använd List.sumBy med en funktion som tar en Transaction och returnerar dess belopp
-// 3. Testa koden
